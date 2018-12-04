099e09befce41e932f63edf9555f8c928118401f
zhentian-wan <answer881215@gmail.com>
2018-12-04 21:16:42 +0200

    cypress tests

---
+++ cypress/e2e/regsiter.js
@@ -0,0 +1,23 @@
+import {userBuilder} from '../support/generate'
+
+describe('should register a new user', () => {
+    it('should register a new user', () => {
+        const user = userBuilder();
+        cy.visit('/')
+            .getByText(/register/i)
+            .click()
+            .getByLabelText(/username/i)
+            .type(user.username)
+            .getByLabelText(/password/i)
+            .type(user.password)
+            .getByText(/submit/i)
+            .click()
+            .url()
+            .should('eq', `${Cypress.config().baseUrl}/`)
+            .window()
+            .its('localStorage.token')
+            .should('be.a', 'string')
+            .getByTestId('username-display', {timeout: 500})
+            .should('have.text', user.username)
+    });
+});
---
+++ cypress/support/generate.js
@@ -0,0 +1,10 @@
+import {build, fake} from 'test-data-bot'
+
+const userBuilder = build('User').fields(
+    {
+        username: fake(f => f.internet.userName()),
+        password: fake(f => f.internet.password())
+    }
+)
+
+export {userBuilder}
